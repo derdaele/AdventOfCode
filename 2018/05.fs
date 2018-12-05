692b64695cb3ddd0e98b5a0d30af75623fbda105
Jérémy Derdaele <jeremy.derdaele@gmail.com>
2018-12-05 10:03:32 +0100

    Add day 5

---
+++ 2018/05.fs
@@ -0,0 +1,34 @@
+﻿module _2018._05
+
+open System
+open Common.Parsing
+
+type InputType = char list
+
+let year, day = 2018, 05
+
+let parse (input: string) : InputType = input.Lines() |> Seq.head |> List.ofSeq
+
+let shouldDestroy (c1: char) (c2: char) = c1 <> c2 && (Char.ToLowerInvariant c1) = (Char.ToLowerInvariant c2)
+
+let rec reduce (polymer: char list) (blacklist: char) acc =
+    match polymer with
+    | c::tl when (Char.ToLowerInvariant c) = blacklist -> reduce tl blacklist acc
+    | c1::c2::tl when shouldDestroy c1 c2 -> reduce tl blacklist acc
+    | hd::tl -> reduce tl blacklist (hd :: acc)
+    | [] -> acc
+
+
+let reaction (polymer: char list) (blacklist: char) =
+    let mutable cur = polymer
+    let mutable next = reduce cur blacklist []
+
+    while cur.Length <> next.Length do
+        cur <- next;
+        next <- reduce cur blacklist []
+
+    next
+
+let solve_1 (input: InputType) = reaction input ' ' |> Seq.length
+
+let solve_2 (input: InputType) = ['a'..'z'] |> List.map (fun blacklist -> reaction input blacklist |> Seq.length) |> List.min
--- AdventOfCode/Program.fs
+++ AdventOfCode/Program.fs
@@ -1,13 +1,20 @@
 ﻿open System
 open System.IO
 
-open _2018._04
+open _2018._05
+
+let duration f x =
+    let timer = new System.Diagnostics.Stopwatch()
+    timer.Start()
+    let returnValue = f x
+    printfn "Elapsed Time: %s" (timer.Elapsed.ToString())
+    returnValue
 
 [<EntryPoint>]
 let main argv =
     let input = File.ReadAllText <| sprintf "%02d/%02d.txt" year day
     let parsed = parse input
-    printfn "Part 1 = %A" <| solve_1 parsed
-    printfn "Part 2 = %A" <| solve_2 parsed
+    printfn "Part 1 = %A" <| duration solve_1 parsed
+    printfn "Part 2 = %A" <| duration solve_2 parsed
 
     0
